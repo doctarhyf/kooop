1517f2da9a96e5627126b51b65d2b233c2db72e9
Docta Rhyf <drrhyf@gmail.com>
2023-09-02 14:53:21 +0200

    working on gui

--- src/pages/PageLogin.jsx
+++ src/pages/PageLogin.jsx
@@ -1,3 +1,16 @@
+import { useEffect, useState } from "react";
+import { LoggedInUser } from "../db/db";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../utils/utils";
+
 export default function PageLogin() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = LoggedInUser();
+
+    if (user) navigate(ROUTES.HOME.path);
+  }, []);
+
   return <div>Login</div>;
 }
